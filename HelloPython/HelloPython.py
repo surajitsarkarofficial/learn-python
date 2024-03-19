e1875250f5ba0cf0116e29fe342433dee737f46d
Surajit Sarkar <surajit_s@blr.trigent.com>
2024-03-19 16:46:26 +0530

    initial commit

---
+++ HelloPython/HelloPython.py
@@ -0,0 +1,10 @@
+print("Hello Python World...")
+# this is a comment
+a = 3
+print(a)
+print("value of a is =", a)
+greet = "Good Morning"
+print(greet)
+
+name, age, percentage = "Suro", 32, 78.89
+print(name, age, percentage)
