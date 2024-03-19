f24c79c51961c799934003511404f83d4fbaceee
Surajit Sarkar <surajit_s@blr.trigent.com>
2024-03-19 17:23:08 +0530

    data-types in-progress

---
+++ HelloPython/Datatypes.py
@@ -0,0 +1,8 @@
+age = 32
+name = "Surajit"
+# to print these values together we need to use format
+print("{} {} {}".format("The Student details are", name, age))
+
+marks = 19.5
+
+print("{} {}".format("Data type of marks variable is ",type(marks)))
