64ac2bac393957012be352eefc42349370355b99
Surajit Sarkar <surajit_s@blr.trigent.com>
2024-03-22 14:42:24 +0530

    For loop examples

---
+++ HelloPython/Loops/ForLoop.py
@@ -0,0 +1,20 @@
+# print 1 to 10
+for i in range(1, 11):
+    print(i)
+# print elements of a list
+fruits = ["apple", "banana", "mango", "orange", "pineapple"]
+for fruit in fruits:
+    print(fruit)
+
+# iterate loops by incrementing iterator by 2
+for i in range (0,20,2):
+    print(i)
+
+# print values 0 to 5
+# if we do not mention start value in range then it starts from 0
+for i in range(6):
+    print(i)
+
+# print values of a list based on its index
+for i in range(0,len(fruits)):
+    print("{} {} {} {}".format("The fruit name at index ",i,"is",fruits[i]))
