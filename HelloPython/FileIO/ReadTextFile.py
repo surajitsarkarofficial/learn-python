79a91699d58818fc0fc1e96f2cd9300ce5c7d062
Surajit Sarkar <surajit_s@blr.trigent.com>
2024-03-26 17:24:40 +0530

    method to skip handling file closing

--- HelloPython/FileIO/ReadTextFile.py
+++ HelloPython/FileIO/ReadTextFile.py
@@ -12,3 +12,8 @@ for line in lines:
     print(line)
 file.close()
 
+#if we don't want to handle closing of file
+with open('dummy.txt') as file :
+    for line in file.readlines():
+        print(line)
+
