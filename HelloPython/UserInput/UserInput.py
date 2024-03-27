b743591fc784f204561c6ec52a7071efab5c3929
Surajit Sarkar <surajit_s@blr.trigent.com>
2024-03-27 15:24:06 +0530

    Added more validations to user input

--- HelloPython/UserInput/UserInput.py
+++ HelloPython/UserInput/UserInput.py
@@ -2,8 +2,11 @@ print('Enter your name')
 name=input()
 print('Enter you Gender (M/F).')
 gender=input()
+if gender not in ['M','F']:
+    raise Exception("Invalid gender...")
 print('Enter Phone Number')
 phn=input()
+phn=int(phn)
 if len(phn) !=10:
     raise Exception("Invalid Phone number...")
 print('Details entered are --- ')
