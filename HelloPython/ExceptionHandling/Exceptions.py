c82f01819f3f3669486c281c79ca28db3cfc3cf2
Surajit Sarkar <surajit_s@blr.trigent.com>
2024-03-27 14:43:18 +0530

    exception handling

---
+++ HelloPython/ExceptionHandling/Exceptions.py
@@ -0,0 +1,50 @@
+try:
+    result = 100/0
+
+except:
+    print("Exception occurred and handled...")
+
+
+# throw an exception
+"""
+threshold = 11
+
+if threshold<=10:
+    print("Within threshold limit...")
+
+else:
+    raise Exception("Threshold limit exceeded...")
+"""
+# handle the exception being thrown
+
+try:
+    # throw an exception
+    threshold = 11
+
+    if threshold <= 10:
+        print("Within threshold limit...")
+
+    else:
+        raise Exception("Threshold limit exceeded...")
+except Exception as e:
+    print(e)
+
+# handle run time exception
+try:
+    with open('myfile.txt') as file:
+        file.read()
+except Exception as e:
+    print(e)
+
+# finally
+name = "Ro"
+try:
+    if len(name) < 3:
+        raise Exception("Name must be atleast 3 characters")
+    else:
+        print("Name is valid..")
+except Exception as e:
+    print(e)
+finally:
+    print("Name has been validated succesfully...")
+
